6832ee01eb28ed2513c9c3a5a05adef4c059c9e9
thesoncriel <theson.criel@gmail.com>
2020-04-26 20:24:01 +0900

    chore: 윈도 개발 환경에서 환경변수 파일 복사 문제 수정

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,26 @@
 /* eslint-disable global-require, import/no-dynamic-require */
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+
+// const gulpCopy = require('gulp-copy');
 // const fs = require('fs');
+
+const aEnvStr = [
+  'dev',
+  'test',
+  'production'
+];
+
+aEnvStr.forEach(name => {
+  gulp.task('cp:' + name, () => {
+    return gulp
+      .src('./environments/.env.' + name)
+      .pipe(rename('.env'))
+      .pipe(gulp.dest('./'));
+  });
+});
+
+
 
 // 도커 베포
 // require('./gulp/docker');
