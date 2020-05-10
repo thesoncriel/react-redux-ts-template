ead0c92bf052863c53d13d4da7bed3a38a309b23
thesoncriel <theson.criel@gmail.com>
2020-05-11 01:01:35 +0900

    fix: 샘플 리듀서 병합으로 인한 오류 수정

--- src/modules/sample/selectors/sample.selector.ts
+++ src/modules/sample/selectors/sample.selector.ts
@@ -6,27 +6,29 @@ import { AppState } from '../../../entries/stores';
  */
 const selSample = (state: AppState) => state.sample;
 
+const selBasic = (state: AppState) => selSample(state).basic;
+
 /**
  * 셀렉터: 샘플 목록 데이터를 가져온다.
  * @param state
  */
-export const selSampleItems = (state: AppState) => selSample(state).items;
+export const selSampleItems = (state: AppState) => selBasic(state).items;
 
 /**
  * 셀렉터: 샘플 링크 목록을 가져온다.
  * @param state
  */
-export const selSampleLinkList = (state: AppState) => selSample(state).linkList;
+export const selSampleLinkList = (state: AppState) => selBasic(state).linkList;
 
 /**
  * 셀렉터: 샘플 목록 데이터의 전체 개수.
  * @param state
  */
 export const selSampleTotalCount = (state: AppState) =>
-  selSample(state).totalCount;
+  selBasic(state).totalCount;
 
 /**
  * 셀렉터: 샘플 목록 불러오는 중인지의 여부.
  * @param state
  */
-export const selSampleLoading = (state: AppState) => selSample(state).loading;
+export const selSampleLoading = (state: AppState) => selBasic(state).loading;
