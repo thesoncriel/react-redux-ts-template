4e65340a687ecc02fb71f22bf31467371964e291
theson <theson@styleshare.kr>
2020-05-20 18:31:30 +0900

    fix: Slick 내부에 anchor 가 있으면 href 수행 안되는 문제 수정

--- src/modules/sample/components/organisms/JumbotronSlider.tsx
+++ src/modules/sample/components/organisms/JumbotronSlider.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, createRef, useEffect } from 'react';
 import Slider from 'react-slick';
 import { Jumbotron } from '../molecules';
 import { serializeParams } from '../../../../util';
@@ -8,16 +8,33 @@ import { JumbotronListProps } from './JumbotronList';
  * 모바일 모드일 때 슬라이더로 보여주는 점보트론 컴포넌트.
  * @param props
  */
-export const JumbotronSlider: FC<JumbotronListProps> = props => (
-  <Slider>
-    {props.items.map((datum, idx) => (
-      <Jumbotron
-        key={idx}
-        name={datum.name}
-        link={serializeParams(datum, true)}
-      >
-        {datum.name}
-      </Jumbotron>
-    ))}
-  </Slider>
-);
+export const JumbotronSlider: FC<JumbotronListProps> = props => {
+  const refSlider = createRef<any>();
+
+  useEffect(() => {
+    if (refSlider.current) {
+      refSlider.current.innerSlider.clickable = true;
+    }
+  }, [refSlider]);
+
+  const handleSwipe = (e: any) => {
+    if (refSlider.current) {
+      refSlider.current.innerSlider.clickable = true;
+    }
+  };
+
+  return (
+    <Slider swipeToSlide={true} onSwipe={handleSwipe} ref={refSlider}>
+      {props.items.map((datum, idx) => (
+        <Jumbotron
+          key={idx}
+          name={datum.name}
+          link={serializeParams(datum, true)}
+        >
+          {datum.name}
+        </Jumbotron>
+      ))}
+      <a href="https://skbt.co.kr">마이홈!!</a>
+    </Slider>
+  );
+};
